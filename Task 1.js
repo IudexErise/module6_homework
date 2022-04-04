46b26b6a748861b8a445d55fdf396e313e7aa556
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2022-04-04 12:26:46 +0300

    task 1-2

---
+++ Task 1.js
@@ -0,0 +1,28 @@
+// Task 6.1
+
+const arr = [0, 3, 8, 9, 12, 14, 5, 20, 0, 'dsaad', null];
+let oddCounter = 0;
+let evenCounter = 0;
+let zeroCounter = 0;
+
+function countArrayElements() {
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 0) {
+    zeroCounter += 1
+  } else if (typeof(arr[i]) !== 'number') {
+    console.log(`This is not a number: ${arr[i]}`)
+  } else if (arr[i] % 2 === 0) {
+    evenCounter += 1
+  } else {
+    oddCounter +=1
+  }
+}
+console.log(`There are ${evenCounter} even numbers`)
+console.log(`There are ${oddCounter} odd numbers`)
+console.log(`There are ${zeroCounter} zeros`)
+}
+countArrayElements();
+
+
+
+
---
+++ Task 2.js
@@ -0,0 +1,21 @@
+// Task 6.2
+
+function checkPrimeNumber(a) {
+  if (a > 1000 ) {
+    console.log('Enter number less than 1000');
+  } else if (a === 0 || a === 1) {
+    console.log('0 and 1 are not prime numbers');
+  } else if (a === 3) {
+    console.log('3 is prime number');
+  } else if (a === 5) {
+    console.log('3 is prime number');
+  } else if (a === 7) {
+    console.log('3 is prime number');
+  } else if (a % 2 !== 0 && a % 3 !== 0 && a % 5 !== 0 && a % 7 !== 0) {
+    console.log('The number is prime');
+  } else {
+    console.log('The number is not prime');
+  }
+    }
+  
+  checkPrimeNumber(789);  
